cbf97e2cc18b6f40cca560c2b7cc9d3354c6bc87
noodles4u <louisdesmett@gmail.com>
2023-09-19 12:15:59 +0200

    first fetch from strapi api

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,3 +1,32 @@
-export default function Book(){
-    return <h1> big test </h1>
+async function getBooks(){
+    const res = await fetch('http://localhost:1337/api/books?populate=*');
+    const data = await res.json();
+
+    return data?.data as any[]
+    
+}
+
+export default async function Book(){
+    
+    const books = await getBooks()
+
+    return (
+        <section>
+            <h1>0x0's catalog</h1>
+     
+            {books?.map((book)=>{
+             return (
+                <div key={book?.id + book?.attributes.Title}>
+           <h2>{book?.attributes.Title}</h2>
+            <div>
+            {book?.attributes?.categories?.data.map((cat)=>{
+                return <p  key={cat?.attributes.tags}>{cat?.attributes?.tags}</p>
+            })}
+            </div>
+            </div>
+            )
+              })}
+        </section>
+        
+        )
 }
