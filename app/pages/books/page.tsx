977c51b50fa6215c7184a1ab81099983e60293d8
noodles4u <louisdesmett@gmail.com>
2023-09-20 23:58:44 +0200

    use client

--- app/pages/books/page.tsx
+++ app/pages/books/page.tsx
@@ -1,59 +1,41 @@
-"use client"
-
-import React, { useEffect, useState } from "react"
-
 async function getBooks() {
   const res = await fetch(process.env.apiURI as string)
   const data = await res.json()
   return data?.data as any[]
 }
 
-const Book = () => {
-  const [books, setBooks] = useState<any[]>([])
-
-  useEffect(() => {
-    const fetchBooks = async () => {
-      try {
-        const fetchedBooks = await getBooks()
-        setBooks(fetchedBooks)
-      } catch (error) {
-        console.error("Error fetching books:", error)
-      }
-    }
-
-    fetchBooks()
-  }, [])
+export default async function Book() {
+  const books = await getBooks()
 
   return (
     <section>
       <h1>0x0s catalog</h1>
 
       {books?.map((book: any) => {
-        const title = book?.attributes?.Title || ""
-        const imageUrl =
-          book?.attributes?.images?.data?.attributes?.formats?.medium?.url || ""
-
         return (
-          <div key={book?.id + title}>
-            <h2>{title}</h2>
+          <div key={book?.id + book?.attributes?.Title}>
+            <h2>{book?.attributes?.Title}</h2>
 
             <div>
               <div key={book?.attributes?.images?.data?.attributes?.name}>
-                <img className="p-8" src={imageUrl} alt={title} />
+                <img
+                  className="p-8"
+                  src={
+                    book?.attributes?.images?.data?.attributes?.formats?.medium
+                      ?.url
+                  }
+                />
               </div>
             </div>
 
             <div>
-              {book?.attributes?.categories?.data.map(
-                (cat: any, index: number) => {
-                  const tags = cat?.attributes?.tags || ""
-                  return (
-                    <p key={index} className="pl-8">
-                      {tags}
-                    </p>
-                  )
-                }
-              )}
+              {book?.attributes?.categories?.data.map((cat: any) => {
+                return (
+                  <p key={cat?.attributes?.tags} className="pl-8">
+                    {cat?.attributes?.tags}
+                  </p>
+                )
+              })}
             </div>
           </div>
         )
@@ -61,5 +43,3 @@ const Book = () => {
     </section>
   )
 }
-
-export default Book
