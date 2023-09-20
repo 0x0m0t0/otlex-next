8d176f63ce0115beebe8f84f1989325d29570e43
noodles4u <louisdesmett@gmail.com>
2023-09-21 00:15:57 +0200

    fix pagees/book

--- app/pages/books/page.tsx
+++ app/pages/books/page.tsx
@@ -1,14 +1,11 @@
-"use client"
-
 async function getBooks() {
-  try {
-    const res = await fetch(process.env.apiURI as string)
-    const data = await res.json()
-    return data?.data || [] // Return an empty array if data is undefined
-  } catch (error) {
-    console.error("Error fetching books:", error)
-    return [] // Return an empty array in case of an error
-  }
+  const res = await fetch(process.env.apiURI as string, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+  const data = await res.json()
+  return data?.data as any[]
 }
 
 export default async function Book() {
@@ -18,36 +15,35 @@ export default async function Book() {
     <section>
       <h1>0x0s catalog</h1>
 
-      {books.map((book) => (
-        <div key={book.id + (book.attributes?.Title || "")}>
-          <h2>{book.attributes?.Title || "Unknown Title"}</h2>
+      {books?.map((book: any) => {
+        return (
+          <div key={book?.id + book?.attributes?.Title}>
+            <h2>{book?.attributes?.Title}</h2>
 
-          <div>
-            <div
-              key={
-                book.attributes?.images?.data?.attributes?.name || "default-key"
-              }
-            >
-              <img
-                className="p-8"
-                src={
-                  book.attributes?.images?.data?.attributes?.formats?.medium
-                    ?.url || "other image"
-                }
-                alt="Book Image"
-              />
+            <div>
+              <div key={book?.attributes?.images?.data?.attributes?.name}>
+                <img
+                  className="p-8"
+                  src={
+                    book?.attributes?.images?.data?.attributes?.formats?.medium
+                      ?.url
+                  }
+                />
+              </div>
+            </div>
+
+            <div>
+              {book?.attributes?.categories?.data.map((cat: any) => {
+                return (
+                  <p key={cat?.attributes?.tags} className="pl-8">
+                    {cat?.attributes?.tags}
+                  </p>
+                )
+              })}
             </div>
           </div>
-
-          <div>
-            {book.attributes?.categories?.data?.map((cat: any) => (
-              <p key={cat?.attributes?.tags || "default-tag"} className="pl-8">
-                {cat?.attributes?.tags || "Unknown Category"}
-              </p>
-            ))}
-          </div>
-        </div>
-      ))}
+        )
+      })}
     </section>
   )
 }
