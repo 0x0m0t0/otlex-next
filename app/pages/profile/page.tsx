f3c9fb9b2831ad0e1cec957d943d421a1c643d99
noodles4u <louisdesmett@gmail.com>
2023-09-22 16:06:23 +0200

    clean up profile from dummy.json

--- app/pages/profile/page.tsx
+++ app/pages/profile/page.tsx
@@ -1,27 +1,7 @@
-async function getTest() {
-  const res = await fetch("https://dummyjson.com/products")
-  const data = await res.json()
-
-  return data?.products as any[]
-}
-
-export default async function Test() {
-  const test = await getTest()
-
+export default function Profile() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1>0x0s catalog</h1>
-
-        {test?.map((item: any) => {
-          return (
-            <div key={item?.id}>
-              <h2>{item?.title}</h2>
-              <img src={item?.thumbnail} alt={item?.title} />
-            </div>
-          )
-        })}
-      </div>
+      <h1 className="text-2xl text-center p-2">Welcome to your Profile</h1>
     </section>
   )
 }
