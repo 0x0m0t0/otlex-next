00e8df1210ad67b53bffa66035be54e91e4b0518
noodles4u <louisdesmett@gmail.com>
2023-09-28 12:33:08 +0200

    list view + image view with localstorage

--- app/pages/books/view.tsx
+++ app/pages/books/view.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 
 import ImageView from "./imageView"
 import ListView from "./listView"
@@ -11,19 +11,46 @@ interface Props {
 }
 
 const View: React.FC<Props> = ({ books }) => {
-  const [view, setView] = useState<boolean>(false)
-  console.log(view)
+  const [view, setView] = useState<boolean>()
+
+  const toggle = () => {
+    console.log("this is the toggle")
+    setView((prevIsClicked) => !prevIsClicked)
+    localStorage.setItem("view", (!view).toString())
+  }
+  const local = () => {
+    console.log("this the local")
+    setView(!view)
+    localStorage.setItem("view", (!view).toString())
+  }
+
   console.log(books)
+
+  useEffect(() => {
+    if (localStorage.getItem("view") === undefined) {
+      setView(false)
+    } else {
+      if (localStorage.getItem("view") === "true") {
+        setView(true)
+      } else {
+        setView(false)
+      }
+    }
+  }, [])
+
   return (
     <>
-      <button onClick={() => setView(true)}>List</button>
-      <p>hello {view.toString()}</p>
+      <button onClick={toggle}>List</button>
 
       {books?.map((book, i) =>
-        view ? (
-          <ListView key={book?.id + book?.date_created} book={book} i={i} />
+        view != undefined ? (
+          view === true ? (
+            <ListView key={book?.id + book?.date_created} book={book} i={i} />
+          ) : (
+            <ImageView key={book?.id + book?.date_created} book={book} i={i} />
+          )
         ) : (
-          <ImageView key={book?.id + book?.date_created} book={book} i={i} />
+          <></>
         )
       )}
     </>
