ba1abd6d3d6d30a1ac0e68ae7f4541a297eec473
noodles4u <louisdesmett@gmail.com>
2023-09-28 15:27:26 +0200

    sorter asc/desc on books array

--- app/pages/books/view.tsx
+++ app/pages/books/view.tsx
@@ -13,16 +13,38 @@ interface Props {
 
 const View: React.FC<Props> = ({ books }) => {
   const [view, setView] = useState<boolean>()
+  const [sorted, setSorted] = useState<Books[]>([])
+  const [clicked, setClicked] = useState<boolean>()
 
   const toggle = () => {
     console.log("this is the toggle")
     setView((prevIsClicked) => !prevIsClicked)
     localStorage.setItem("view", (!view).toString())
   }
-  //   sort(books).asc()
+
+  const sorting = (sorterClick: any, clicked) => {
+    if (clicked === true) {
+      const sorted = sort(books).asc((book) => book[sorterClick])
+      setSorted(sorted)
+    } else {
+      const sorted = sort(books).desc((book) => book[sorterClick])
+      setSorted(sorted)
+    }
+  }
+
+  const handleClick = (e: any) => {
+    const clickedValue = e.target.getAttribute("data-value")
+    setClicked(!clicked)
+    console.log("Clicked value:", clickedValue)
+
+    sorting(clickedValue, clicked)
+  }
+
   console.log(books)
 
   useEffect(() => {
+    setClicked(false)
+    setSorted(sort(books).asc((book) => book?.Name))
     if (localStorage.getItem("view") === undefined) {
       setView(false)
     } else {
@@ -34,12 +56,21 @@ const View: React.FC<Props> = ({ books }) => {
     }
   }, [])
 
-  const sorted = sort(books).asc((book) => book?.Name)
-
   return (
     <>
       <button onClick={toggle}>List</button>
-
+      <div className="flex p-2 items-center justify-between border m-1">
+        <p>i</p>
+        <p data-value="Name" onClick={handleClick} className="text-sm w-1/4">
+          Title
+        </p>
+        <p data-value="Authors" onClick={handleClick} className="text-sm w-1/4">
+          Author
+        </p>
+        <p data-value="Year" onClick={handleClick} className="text-sm w-1/4">
+          Year
+        </p>
+      </div>
       {sorted?.map((book, i) =>
         view != undefined ? (
           view === true ? (
